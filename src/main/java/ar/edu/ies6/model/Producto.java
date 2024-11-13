7e4047f0d56838d4e735af9f0c7e6b35032929e8
Estudiante <Estudiante@192.168.1.10>
2024-11-13 13:09:42 -0300

    model-producto-evelyn martinez

---
+++ src/main/java/ar/edu/ies6/model/Producto.java
@@ -0,0 +1,67 @@
+package ar.edu.ies6.model;
+
+public class Producto {
+	
+	//atributos
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	private int stock;
+	private boolean estado;
+	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+
+	
+}
